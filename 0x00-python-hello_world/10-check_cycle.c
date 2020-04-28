257aec83b5f98ccc2679320845885c7e85ff0658
Wilds Rincon <1588@holbertonschool.com>
2020-04-27 21:33:23 -0500

    Adding file 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: singly linked list to check
+ *
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr1;
+	listint_t *ptr2;
+
+	if (list == NULL)
+		return (0);
+
+	ptr1 = list;
+	ptr2 = list;
+	while (ptr2->next != NULL)
+	{
+		if (ptr2->next->next == NULL)
+			return (0);
+		ptr2 = ptr2->next->next;
+		ptr1 = ptr1->next;
+		if (ptr2 == slow)
+			return (1);
+	}
+	return (0);
+}
