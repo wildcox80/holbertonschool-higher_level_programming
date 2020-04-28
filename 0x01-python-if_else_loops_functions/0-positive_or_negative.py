e3881be0baead64057e0f35e6dc1febe36d9890b
Wilder Rincon <1588@holbertonschool.com>
2020-04-28 18:47:50 -0500

    Delete file 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print("{:d} is Positive".format(number))
-elif number == 0:
-    print("{:d} is Zero".format(number))
-else:
-    print("{:d} is Negative".format(number))
