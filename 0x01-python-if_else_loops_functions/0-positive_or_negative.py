e8ce3c752e1334f67b1ed54523b4f1db8f314bab
Wilds Rincon <1588@holbertonschool.com>
2020-04-28 19:08:23 -0500

    Adding folder project

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    num = "is positive"
+elif number == 0:
+    num = "is nero"
+else:
+    num = "is negative"
+
+print("{:d} {:s}".format(number, num))
