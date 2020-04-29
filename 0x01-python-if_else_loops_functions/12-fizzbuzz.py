58f6633f2cc8635e9e25d0aaf86c708d4215725e
Wilds Rincon <1588@holbertonschool.com>
2020-04-28 22:29:19 -0500

    Adding file 12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 != 0 and i % 5 != 0:
+            print(i, end=" ")
+            continue
+        elif i % 3 == 0:
+            print("Fizz", end="")
+        elif i % 5 == 0:
+            print("Buzz", end="")
+        print(" ", end="")
