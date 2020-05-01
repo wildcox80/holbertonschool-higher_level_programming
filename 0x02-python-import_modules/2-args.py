85431bd3831a2f6657674e8301db1197f84505cc
Wilds Rincon <1588@holbertonschool.com>
2020-04-30 20:16:01 -0500

    Adding file 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    from sys import argv
+    num = len(argv) - 1
+    print("{:d} argument{}".format(num, "" if num == 1 else "s"), end="")
+    print("{}".format("." if num == 0 else ":"))
+    if num > 0:
+        for i in range(1, num + 1):
+            print("{:d}: {}".format(i, argv[i]))
