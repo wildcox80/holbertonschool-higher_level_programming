6c2c78cca12d683884a83f5276bb002ace6c7323
Wilds Rincon <1588@holbertonschool.com>
2020-04-30 21:08:48 -0500

    Adding file 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    sum_all = len(argv) - 1
-    res = 0
-    for i in range(1, sum_all + 1):
-        res += int(argv[i])
-    print("{:d}".format(res))
+    num = len(argv) - 1
+    sum = 0
+    for i in range(1, num + 1):
+        sum += int(argv[i])
+    print("{:d}".format(sum))
