d5968d1d0f645ccd96cab76cf671e4bcfb7352f9
Wilds Rincon <1588@holbertonschool.com>
2020-05-05 21:53:24 -0500

    Adding file 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,10 +4,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a, b = 0, 0
     elif len(tuple_a) == 1:
         a, b = tuple_a[0], 0
+    else:
         a, b = tuple_a[0:2]
     if len(tuple_b) == 0:
         c, d = 0, 0
     elif len(tuple_b) == 1:
         c, d = tuple_b[0], 0
+    else:
         c, d = tuple_b[0:2]
     return (a + c, b + d)
