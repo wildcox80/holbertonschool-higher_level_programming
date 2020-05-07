ea0d418e01bd4dd4ac41bfdae38768b28c608812
Wilds Rincon <1588@holbertonschool.com>
2020-05-07 18:38:48 -0500

    Adding file 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(my_dict, key=""):
-    del my_dict[key]
-    return my_dict
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
