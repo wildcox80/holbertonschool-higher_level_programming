a93affef26b57d383522363a6baf74285fbf251e
Wilds Rincon <1588@holbertonschool.com>
2020-05-07 15:05:08 -0500

    Adding file 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:return [list(map(lambda x: x**2, row)) for row in matrix]
+    if matrix:
+        return [list(map(lambda x: x**2, row)) for row in matrix]
