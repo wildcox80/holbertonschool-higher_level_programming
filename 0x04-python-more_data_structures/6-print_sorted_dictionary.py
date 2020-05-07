3cbf642df007a4f6a0222548c26a4121dafbb393
Wilds Rincon <1588@holbertonschool.com>
2020-05-07 18:18:26 -0500

    Adding file 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
-        print("{}: {}".format(key, value))
+def print_sorted_dictionary(a_dictionary):
+    for key, val in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, val))
