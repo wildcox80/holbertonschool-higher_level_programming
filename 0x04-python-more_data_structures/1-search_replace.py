a1ec98f31322c2ded2efe6d6db417b24cf32acd0
Wilds Rincon <1588@holbertonschool.com>
2020-05-07 15:51:11 -0500

    Adding file 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [list(map(lambda i: i if i != search else replace, my_list))]
+    return (list(map(lambda i: i if i != search else replace, my_list)))
