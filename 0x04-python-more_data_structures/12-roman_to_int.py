6dc22e59538794795b1622b5ee441110ccfb41c2
Wilds Rincon <1588@holbertonschool.com>
2020-05-07 21:58:03 -0500

    Adding file 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
-    if type(roman_string) is not str or len(roman_string) is 0:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
+    pre, sum = 0, 0
 
-    roman_numbers = {
-        'I': 1, 'V': 5,
-        'X': 10, 'L': 50,
-        'C': 100, 'D': 500,
-        'M': 1000
-    }
-    output, loops, prev = 0
-
-    for key in roman_string:
-        if loops > 0 and prev < roman_numbers[key]:
-            output -= prev
-            output = output + (roman_numbers[key] - prev)
-        else:
-            output = output + roman_numbers[key]
-
-        loops += 1
-        prev = roman_numbers[key]
-    return output
+    for i in roman_string:
+        sum += roman[i] if roman[i] <= pre else roman[i] - pre * 2
+        pre = roman[i]
+    return sum
