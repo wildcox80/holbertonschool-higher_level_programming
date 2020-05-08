896cfa2391ff95b98a127a302b33cc148d809567
Wilds Rincon <1588@holbertonschool.com>
2020-05-07 19:09:29 -0500

    Adding file 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return
-    return max(a_dictionary) 
+    new_dict = []
+    for new in a_dictionary:
+        new_dict.append(new)
+    return max(new_dict)
