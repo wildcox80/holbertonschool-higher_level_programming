1d04a5fdfbb2160083ea717b066e51736ff3b485
Wilds Rincon <1588@holbertonschool.com>
2020-05-18 14:08:22 -0500

    Adding file 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for item in range(x):
-            print("{}".format(my_list[item]), end="")
+    count = 0
+    for item in range(x):
+        try:
+            print(my_list[item], end="")
             count += 1
-    except IndexError:
-        pass
+        except IndexError:
+            break
     print()
     return count
