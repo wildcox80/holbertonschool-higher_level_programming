b9090530357b6e6fb060bb69340df7d954ddeaa7
Wilds Rincon <1588@holbertonschool.com>
2020-05-18 14:49:01 -0500

    Adding file 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i, ints = 0
-    for i in range(x):
+    while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
             ints += 1
