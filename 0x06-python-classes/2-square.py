2c3ad057e2b504dafb762f49339064b433d57ab1
Wilds Rincon <1588@holbertonschool.com>
2020-05-19 16:16:37 -0500

    Adding file 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""1-square.py: class Square that defines a square
+private instantiation attribute of size"""
+
+
 class Square:
-    """ Square class with validate private instance """
-    def __init__(self, size = 0):
-        """
-        Arguments:
-            size: size of square
-        """
+    """Creates Square type"""
+
+    def __init__(self, size=0):
+        """Initializes Square with size"""
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        
-        self.size = size
+
+        self.__size = size
