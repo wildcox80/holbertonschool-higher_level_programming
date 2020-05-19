45ccffa5f466b1ca3f562bd4d39adf8a5db0f390
Wilds Rincon <1588@holbertonschool.com>
2020-05-19 15:55:12 -0500

    Adding file 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""1-square.py: class Square that defines a square by: (based on 0-square.py)"""
+
+
 class Square:
     """ Square class with validate private instance """
     def __init__(self, size):
