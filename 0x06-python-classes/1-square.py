878aaef386098cf152194261c19c97471453208d
Wilds Rincon <1588@holbertonschool.com>
2020-05-19 15:41:35 -0500

    Adding file 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """ Square class with validate private instance """
+    def __init__(self, size):
+        """
+        Arguments:
+            size: size of square
+        """
+        size = 0
+        self.size = size
