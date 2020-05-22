452213e4c12d4ee0cffab66c9ae07fa05889ffa8
Wilds Rincon <1588@holbertonschool.com>
2020-05-22 17:15:47 -0500

    Adding file 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -17,8 +17,9 @@ def say_my_name(first_name, last_name=""):
         TypeError: [first_name must be a string]
         TypeError: [last_name must be a string]
     """
-    if type(first_name) is not str:
-        raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    if first_name:
+        if type(first_name) is not str:
+            raise TypeError("first_name must be a string")
+        if type(last_name) is not str:
+            raise TypeError("last_name must be a string")
+        print("My name is {} {}".format(first_name, last_name))
