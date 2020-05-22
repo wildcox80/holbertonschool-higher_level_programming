1f34939350e46f2d8adaa6a2774bb503110f6079
Wilds Rincon <1588@holbertonschool.com>
2020-05-22 16:44:57 -0500

    Adding file 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,11 +17,11 @@ def print_square(size):
         TypeError: [size must be >= 0]
         TypeError: [size must be an integer]
     """
-    if type(size) != int:
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if type(size) == float and type(size) <= 0:
+    if type(size) is float and type(size) < 0:
         raise TypeError("size must be an integer")
     else:
         print("{}\n".format("#" * size) * size, end="")
