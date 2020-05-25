207d030afc97967fde90d89b2602e6405541cf8c
Wilds Rincon <1588@holbertonschool.com>
2020-05-25 18:18:33 -0500

    Adding file 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,13 +4,15 @@
 
 
 class Rectangle:
-    """Creates  rectangle type"""
+    """ Define Class with validated private instance
+        attributes width and height
+    """
 
     def __init__(self, width=0, height=0):
         """Initializes rectangle size"""
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
