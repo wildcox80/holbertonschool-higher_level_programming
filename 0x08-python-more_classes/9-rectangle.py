b71214154ab36fb4bb5c1624d18bcf9a194b89fb
Wilds Rincon <1588@holbertonschool.com>
2020-05-25 18:53:07 -0500

    Adding file 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -113,13 +113,10 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """Square
-            
-            Keyword Arguments:
-            size {int} -- size of the square (default: {0})
-            
-            Returns:
-            class -- Return a class
+        """
+            Returns new Rectangle instance with width == height == size
+            Parameters:
+            size: height and width of new instance
         """
 
-        return cls(size, size)
+        return Rectangle(size, size)
