50a3b5ba691590c38bc9c10f6c04c8077471fee0
Wilds Rincon <1588@holbertonschool.com>
2020-05-25 20:16:04 -0500

    Adding file 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -96,6 +96,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print('Bye rectangle...')
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """ Returns the bigger Rectangle or rect_1 if they are equal
             Raises:
