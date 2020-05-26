c74b330473296a583d476b5ce487681b9c28d7f4
Wilds Rincon <1588@holbertonschool.com>
2020-05-25 20:24:12 -0500

    Adding file 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -23,8 +23,8 @@ class Rectangle:
             height: height of rectangle
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
