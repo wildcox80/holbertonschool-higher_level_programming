523e0491d399cb393e7f1f09a1044af1d09e2134
Wilds Rincon <1588@holbertonschool.com>
2020-05-25 19:48:41 -0500

    Adding file 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -106,7 +106,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() < rect_2.area():
+        if rect_2.area() > rect_1.area():
             return rect_2
         return rect_1
 
