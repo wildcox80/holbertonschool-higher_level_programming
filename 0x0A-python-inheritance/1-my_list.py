2a0089230154fc453264e7a205eac78a7e26e34b
Wilds Rincon <1588@holbertonschool.com>
2020-06-02 15:27:26 -0500

    Adding file 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    1-my_list.py: Write a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """ Class MyList inherits from list"""
+    
+
+    def print_sorted(self):
+        """
+            Print list my_list    
+        """
+        print(sorted(self))
