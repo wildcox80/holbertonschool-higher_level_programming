afe8e824c795ba11163fc3baf4fdd1d8242ea379
Wilds Rincon <1588@holbertonschool.com>
2020-06-02 18:36:54 -0500

    Adding changes file 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""
-    7-base_geometry: Write a class BaseGeometry
-    (based on 6-base_geometry.py)
-"""
+""" 7-base_geometry.py: This module creates an class BaseGeometry """
 
 
-class BaseGeometry():
-    """ Class BaseGeometry """
-
+class BaseGeometry:
+    """Class BaseGeometry"""
     def area(self):
         """Raises an Exception - method not implemented"""
 
-        raise Exception("area() is no implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-            Validates a given value is a positive int
-            Param:
-                name: name of variable to validate
-                value: value to validate
-            Raises:
-                TypeError: if value is not an int
-                ValueError: if value is negative
+        """Validates a given value is a positive int
+        Param:
+            name: name of variable to validate
+            value: value to validate
+        Raises:
+            TypeError: if value is not an int
+            ValueError: if value is negative
         """
 
         if type(value) is not int:
-            raise TypeError("{} must be a integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greather than 0".format(name))
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0". format(name))
