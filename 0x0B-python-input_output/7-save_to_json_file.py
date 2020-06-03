34a27da7b65e7943dd0cf10dcf388732c7fa7018
Wilds Rincon <1588@holbertonschool.com>
2020-06-03 18:26:17 -0500

    Adding file 7-save_to_json_file.py

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -10,4 +10,4 @@ import json
 
 def save_to_json_file(my_obj, filename):
     with open(filename, mode='w', encoding='UTF-8') as f:
-        json.dump(my_obj, f)
+        f.write(json.dumps(my_obj))
