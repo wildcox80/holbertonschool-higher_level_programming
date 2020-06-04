8220c8384b29ea087ed54cf429f33d0399736a91
Wilds Rincon <1588@holbertonschool.com>
2020-06-03 20:31:27 -0500

    Adding file 10-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    11-student.py: Create class student
+"""
+
+
+class Student(self):
+    """
+        Define class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            Define obj class Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+            Return dict class Student
+        """
+        return self.__dict__
