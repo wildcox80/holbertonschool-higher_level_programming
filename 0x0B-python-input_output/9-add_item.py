f592ec0290c20e3e536a936b4b52255864f11636
Wilds Rincon <1588@holbertonschool.com>
2020-06-03 19:57:59 -0500

    Adding file 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Python function that add arguments to a Python list
+    function for read and safe argv in
+    Json format in file
 """
 import sys
-import os
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
-
+filename = "add_item.json"
 my_list = []
-
-if not os.path.exists("./add_item.json"):
-    save_json(my_list, "add_item.json")
-my_list = load_json("add_item.json")
-
-for items in sys.argv[1:]:
-    my_list.append(items)
-save_json(my_list, "add_item.json")
+try:
+    my_list = load_from_json_file(filename)
+except:
+    pass
+finally:
+    for idx in range(1, len(sys.argv)):
+        my_list.append(sys.argv[idx])
+    save_to_json_file(my_list, filename)
