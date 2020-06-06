581cf41e318cc21771df7258960500358bf29766
Wilder Rincon <1588@holbertonschool.com>
2020-06-06 00:54:51 -0500

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
+# jose vallejo <1545@holbertonschool.com>
 """
-    5-text_indentation.py: prints a text with 2 new lines
-    after each of these characters: ., ? and :
+    create funtion that print swuare in # 
+    @size must be only int
+    Return: only print scuare by size*size in #
 """
 
 
 def text_indentation(text):
     """
-    Prints text with 2 lines after each `.`, `?`, and `:`
+        function for split text givend
     """
-    if type(text) is not str:
-        raise TypeError("text must be a string")
-    start = True
-    space_count = 0
-    for c in text:
-        if c == " " and start:
-            continue
-        if c == " ":
-            space_count += 1
-            continue
+
+    chr_match = ['.', '?', ':']
+    new_str = ""
+    if text:
+        if type(text) is str:
+            for i in text:
+                new_str = new_str + i
+                if i in chr_match:
+                    print(new_str.strip())
+                    print("")
+                    new_str = ""
+            print(new_str.strip(), end="")
         else:
-            print("{}".format(" " * space_count), end="")
-            space_count = 0
-        if c == "." or c == "?" or c == ":":
-            print("{}\n".format(c))
-            start = True
-        else:
-            start = False
-            print("{}".format(c), end="")
+            raise TypeError("text must be a string")
