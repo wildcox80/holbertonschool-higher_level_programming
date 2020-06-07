a25ceb89a266ee5c0798ea9dc84dbe6c87bb289b
Wilds Rincon <1588@holbertonschool.com>
2020-06-07 02:40:29 -0500

    Adding changes file square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,13 +3,34 @@
     2-rectangle.py: Model class Rectangle that inherits from Base
     Wilder Rincon : 1588@holbertonschool.com
 """
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
+        """ Initiliaze method square
 
-        super().__init__(id)
-        self.x = x
-        self.y = y
-        self.id = id
+        Args:
+            size ([int]): [size square]
+            x (int, optional): [x value]. Defaults to 0.
+            y (int, optional): [y value]. Defaults to 0.
+            id ([int], optional): [id value]. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns string representation of a square"""
+        return "[Square] ({}) {:d}/{:d} - {:d}"\
+            .format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """ getter method for width """
+        return self.width
+        
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value       
+        
