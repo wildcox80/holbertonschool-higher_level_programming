195a4aee3fe602817fbc9ab139afe39d901569ac
Wilds Rincon <1588@holbertonschool.com>
2020-06-09 16:03:41 -0500

    Adding changes tests file test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,15 +1,83 @@
 #!/usr/bin/python3
 """ unit test for file base.py """
 import unittest
+import pep8
 import io
-import sys
+from io import StringIO
 import json
+import sys
+import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
 
 class TestSquareClass(unittest.TestCase):
+    """
+    Class containing functions to run
+    multiple tests
+    """
+
+    def setUp(self):
+        """
+        function to redirect stdout to check
+        outpute of functions relying on print
+        """
+        sys.stdout = StringIO()
+
+    def tearDown(self):
+        """
+        cleans everything up after running
+        setup
+        """
+        sys.stdout = sys.__stdout__
+
+    def test_pep8_model(self):
+        """
+        Tests for pep8
+        """
+        p8 = pep8.StyleGuide(quiet=True)
+        p = p8.check_files(['models/rectangle.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_pep8_test(self):
+        """
+        Tests for pep8
+        """
+        p8 = pep8.StyleGuide(quiet=True)
+        p = p8.check_files(['tests/test_models/test_rectangle.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_docstring(self):
+        """test if docstring"""
+        self.assertIsNotNone(Base.__doc__)
+
+    def test_00_documentation(self):
+        """
+        Test to see if documentation is
+        created and correct
+        """
+        self.assertTrue(hasattr(Rectangle, "__init__"))
+        self.assertTrue(Rectangle.__init__.__doc__)
+        self.assertTrue(hasattr(Rectangle, "width"))
+        self.assertTrue(Rectangle.width.__doc__)
+        self.assertTrue(hasattr(Rectangle, "height"))
+        self.assertTrue(Rectangle.height.__doc__)
+        self.assertTrue(hasattr(Rectangle, "x"))
+        self.assertTrue(Rectangle.x.__doc__)
+        self.assertTrue(hasattr(Rectangle, "y"))
+        self.assertTrue(Rectangle.y.__doc__)
+        self.assertTrue(hasattr(Rectangle, "area"))
+        self.assertTrue(Rectangle.area.__doc__)
+        self.assertTrue(hasattr(Rectangle, "display"))
+        self.assertTrue(Rectangle.display.__doc__)
+        self.assertTrue(hasattr(Rectangle, "__str__"))
+        self.assertTrue(Rectangle.__str__.__doc__)
+        self.assertTrue(hasattr(Rectangle, "update"))
+        self.assertTrue(Rectangle.update.__doc__)
+        self.assertTrue(hasattr(Rectangle, "to_dictionary"))
+        self.assertTrue(Rectangle.to_dictionary.__doc__)
+
     def test_init_empty(self):
         """Initialize with no arguments"""
         with self.assertRaises(TypeError):
