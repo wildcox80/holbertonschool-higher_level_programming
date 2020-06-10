7cb95aed49dc2b8dff95df47d8f66abe2ce3c356
Wilds Rincon <1588@holbertonschool.com>
2020-06-09 23:47:55 -0500

    Adding changes tests file test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,200 +2,185 @@
 """ unit test for file base.py """
 import unittest
 import pep8
-import io
-from io import StringIO
-import json
-import sys
-import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+import sys
+from io import StringIO
+import json
+import os
+"""This module contains a test suite"""
 
 
-class TestBaseClass(unittest.TestCase):
-    """
-    Class containing functions to run
-    multiple tests
-    """
+class TestBase(unittest.TestCase):
+    """Class containing test suite"""
 
     def setUp(self):
-        """
-        function to redirect stdout to check
-        outpute of functions relying on print
-        """
+        """Redirect stdout"""
         sys.stdout = StringIO()
 
     def tearDown(self):
-        """
-        cleans everything up after running
-        setup
-        """
+        """Cleans stream"""
         sys.stdout = sys.__stdout__
 
     def test_pep8_model(self):
-        """
-        Tests for pep8
-        """
-        p8 = pep8.StyleGuide(quiet=True)
-        p = p8.check_files(['models/rectangle.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        """Tests for pep8"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_pep8_test(self):
-        """
-        Tests for pep8
-        """
-        p8 = pep8.StyleGuide(quiet=True)
-        p = p8.check_files(['tests/test_models/test_rectangle.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        """Tests for pep8"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_docstring(self):
-        """test if docstring"""
+        """Test if docstring exists"""
         self.assertIsNotNone(Base.__doc__)
 
-    def test_00_documentation(self):
-        """
-        Test to see if documentation is
-        created and correct
-        """
-        self.assertTrue(hasattr(Rectangle, "__init__"))
-        self.assertTrue(Rectangle.__init__.__doc__)
-        self.assertTrue(hasattr(Rectangle, "width"))
-        self.assertTrue(Rectangle.width.__doc__)
-        self.assertTrue(hasattr(Rectangle, "height"))
-        self.assertTrue(Rectangle.height.__doc__)
-        self.assertTrue(hasattr(Rectangle, "x"))
-        self.assertTrue(Rectangle.x.__doc__)
-        self.assertTrue(hasattr(Rectangle, "y"))
-        self.assertTrue(Rectangle.y.__doc__)
-        self.assertTrue(hasattr(Rectangle, "area"))
-        self.assertTrue(Rectangle.area.__doc__)
-        self.assertTrue(hasattr(Rectangle, "display"))
-        self.assertTrue(Rectangle.display.__doc__)
-        self.assertTrue(hasattr(Rectangle, "__str__"))
-        self.assertTrue(Rectangle.__str__.__doc__)
-        self.assertTrue(hasattr(Rectangle, "update"))
-        self.assertTrue(Rectangle.update.__doc__)
-        self.assertTrue(hasattr(Rectangle, "to_dictionary"))
-        self.assertTrue(Rectangle.to_dictionary.__doc__)
+    def test_documentation(self):
+        """Test to see if documentation is created and correct"""
+        self.assertTrue(hasattr(Base, "__init__"))
+        self.assertTrue(Base.__init__.__doc__)
+        self.assertTrue(hasattr(Base, "create"))
+        self.assertTrue(Base.create.__doc__)
+        self.assertTrue(hasattr(Base, "to_json_string"))
+        self.assertTrue(Base.to_json_string.__doc__)
+        self.assertTrue(hasattr(Base, "from_json_string"))
+        self.assertTrue(Base.from_json_string.__doc__)
+        self.assertTrue(hasattr(Base, "save_to_file"))
+        self.assertTrue(Base.save_to_file.__doc__)
+        self.assertTrue(hasattr(Base, "load_from_file"))
+        self.assertTrue(Base.load_from_file.__doc__)
 
-    def test_init(self):
-        """Initialize with no arguments"""
+    def test_id(self):
+        """
+        Test to check for id method
+        """
+        Base._Base__nb_objects = 0
         b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(15)
         self.assertEqual(b1.id, 1)
-        b2 = Base()
         self.assertEqual(b2.id, 2)
-        b2 = Base()
-        self.assertEqual(b2.id, 3)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 15)
 
-    def test_init_param(self):
-        """Initialize with id given"""
-        b1 = Base(12)
-        self.assertEqual(b1.id, 12)
-        b2 = Base(-1)
-        self.assertEqual(b2.id, -1)
-        b1 = Base(0)
-        self.assertEqual(b1.id, 0)
-
-    def test_init_mix(self):
-        """Initialize with no id given, id given, no id given"""
-        b1 = Base()
-        self.assertEqual(b1.id, 4)
-        b2 = Base()
-        self.assertEqual(b2.id, 5)
-        b3 = Base(8)
-        self.assertEqual(b3.id, 8)
-        b4 = Base()
-        self.assertEqual(b4.id, 6)
-
-    def test_init_toomany(self):
-        """Initialize with too many arguments"""
-        with self.assertRaises(TypeError):
-            b1 = Base(1, 2)
-
-    def test_init_str(self):
-        """Initialize id with string"""
-        b1 = Base("123")
-        self.assertEqual(b1.id, "123")
-
-    def test_init_dict(self):
-        """Initialize id with dictionary"""
-        b1 = Base({"123": "456"})
-        self.assertEqual(b1.id, {"123": "456"})
-
-    def test_init_tup(self):
-        """Initialize id with tuple"""
-        b1 = Base((1, ))
-        self.assertEqual(b1.id, (1, ))
-
-    def test_init_list(self):
-        """Initialize id with list"""
-        b1 = Base([1])
-        self.assertEqual(b1.id, [1])
-
-    def test_6_from_json_string(self):
+    def test_id_random(self):
         """
-        Test to check from json to string
-        conversion
+        Random arguments passed to check
+        """
+        Base._Base__nb_objects = 0
+        t1 = Base(22)
+        self.assertEqual(t1.id, 22)
+        t2 = Base(-33)
+        self.assertEqual(t2.id, -33)
+        t3 = Base()
+        self.assertEqual(t3.id, 1)
+
+    def test_set_nb_private(self):
+        """Check if nb_objects is private"""
+        b = Base(33)
+        with self.assertRaises(AttributeError):
+            print(b.nb_objects)
+        with self.assertRaises(AttributeError):
+            print(b.__nb_objects)
+
+    def test_dict(self):
+        """Test to check dictionary"""
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        d1 = r1.to_dictionary()
+        j = {'x': 2, 'id': 1, 'y': 8, 'height': 7, 'width': 10}
+        jd = Base.to_json_string([d1])
+        self.assertEqual(d1, j)
+        self.assertEqual(type(d1), dict)
+        self.assertEqual(type(jd), str)
+
+    def test_to_json_string(self):
+        """Test to check for string to json conversion"""
+        Base.__nb_objects = 0
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertTrue(type(Base.to_json_string(None)) is str)
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertTrue(type(Base.to_json_string("[]")) is str)
+        dt1 = {"id": 7, "width": 10, "height": 22, "x": 4, "y": 5}
+        dt2 = {"id": 8, "width": 4, "height": 2, "x": 14, "y": 3}
+        conv = Base.to_json_string([dt1, dt2])
+        self.assertTrue(type(conv) is str)
+        d = json.loads(conv)
+        self.assertEqual(d, [dt1, dt2])
+
+    def test_from_json_string_conversion(self):
+        """Test to check from json to string conversion
         """
         s = '[{"id": 9, "width": 10, "height": 11, "x": 12, "y": 13}, \
-        {"id": 10, "width": 12, "height": 14, "x": 16, "y": 18}]'
+{"id": 10, "width": 12, "height": 14, "x": 16, "y": 18}]'
         js = Base.from_json_string(s)
         self.assertTrue(type(js) is list)
         self.assertEqual(len(js), 2)
 
-    def test_7_from_json_string_empty(self):
-        """
-        Test to check if it works with
-        empty string or none
-        """
+    def test_from_json_string_empty(self):
+        """Test to check if it works with empty string or none"""
         self.assertEqual(Base.from_json_string(""), [])
         self.assertEqual(Base.from_json_string(None), [])
 
-    def test_save_to_file_none(self):
-        """Test save_to_file() with None"""
-        Base.save_to_file(None)
-        with open("Base.json", "r") as f:
-            bases = json.load(f)
-            self.assertEqual(len(bases), 0)
-            self.assertEqual(bases, [])
+    def test_json_file_empty(self):
+        """Test to check from empty"""
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", mode="r") as myFile:
+            self.assertEqual([], json.load(myFile))
 
-    def test_save_to_file_empty(self):
-        """Test save_to_file() with empty list"""
-        Base.save_to_file([])
-        with open("Base.json", "r") as f:
-            bases = json.load(f)
-            self.assertEqual(len(bases), 0)
-            self.assertEqual(bases, [])
+    def test_json_file_none(self):
+        """
+        Test to check from none empty
+        """
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", mode="r") as myFile:
+            self.assertEqual([], json.load(myFile))
 
-    def test_load_from_file(self):
-        """Test load_from_file with non-existant file"""
-        obj = Base.load_from_file()
-        self.assertEqual(obj, [])
+    def test_rect(self):
+        """
+        Test to check for rectangle creation
+        """
+        R1 = Rectangle(4, 5, 6)
+        R1_dict = R1.to_dictionary()
+        R2 = Rectangle.create(**R1_dict)
+        self.assertNotEqual(R1, R2)
 
-    def test_from_json_string_none(self):
-        """Test from_json_string with None"""
-        self.assertEqual(Base.from_json_string(None), [])
+    def test_square(self):
+        """
+        Test to check for square creation
+        """
+        S1 = Square(44, 55, 66, 77)
+        S1_dict = S1.to_dictionary()
+        S2 = Rectangle.create(**S1_dict)
+        self.assertNotEqual(S1, S2)
 
-    def test_save_file_csv_none(self):
-        """Test save None object to csv file"""
-        Base.save_to_file_csv(None)
-        with open("Base.csv", "r", newline='') as f:
-            bases = Base.load_from_file_csv()
-            self.assertEqual(len(bases), 0)
-            self.assertEqual(bases, [])
+    def test_file_rectangle(self):
+        """
+        Test to check if file loads from rect
+        """
+        R1 = Rectangle(33, 34, 35, 26)
+        R2 = Rectangle(202, 2)
+        lR = [R1, R2]
+        Rectangle.save_to_file(lR)
+        lR2 = Rectangle.load_from_file()
+        self.assertNotEqual(lR, lR2)
 
-    def test_save_file_csv_empty(self):
-        """Test save empty list to csv file"""
-        Base.save_to_file_csv([])
-        with open("Base.csv", "r", newline='') as f:
-            bases = Base.load_from_file_csv()
-            self.assertEqual(len(bases), 0)
-            self.assertEqual(bases, [])
-
-    def test_load_from_file_csv(self):
-        """Test loading non-existant file"""
-        obj = Base.load_from_file_csv()
-        self.assertEqual(obj, [])
+    def test_file_square(self):
+        """
+        Test to check if file loads from square
+        """
+        S1 = Square(22)
+        S2 = Square(44, 44, 55, 66)
+        lS = [S1, S2]
+        Square.save_to_file(lS)
+        lS2 = Square.load_from_file()
+        self.assertNotEqual(lS, lS2)
 
 
 if __name__ == "__main__":
