8e46fe4c95cde3e502b5434b709a8e3757f12e93
Wilder Rincon <1588@holbertonschool.com>
2020-07-13 19:01:24 +0000

    Adding changes file 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,3 @@
 #!/bin/bash/env mysql
 -- Create first table in MySql
-CREATE TABLE IF EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(256),
-signup_date DATE);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
