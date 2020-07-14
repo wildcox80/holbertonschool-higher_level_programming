411980c82fea90aa7702d98b23644c8d12e1db4d
Wilder Rincon <1588@holbertonschool.com>
2020-07-14 22:56:24 +0000

    Adding file 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,5 @@
 -- List all genres and the number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_show_genres INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+FROM tv_show_genres INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name ORDER BY number_shows DESC;
