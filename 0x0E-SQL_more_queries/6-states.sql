48e1f7d06ac6f92fb1a39d0f5257d3d9842baf3d
Wilder Rincon <1588@holbertonschool.com>
2020-07-14 20:07:50 +0000

    Adding file 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- create database hbtn_0d_usa with table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256));
