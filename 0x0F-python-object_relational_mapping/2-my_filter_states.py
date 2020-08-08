81f82acda3602e23495dae774d84e39f607fea97
Wilder Rincon <1588@holbertonschool.com>
2020-08-08 17:27:53 -0500

    Adding file 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N """
+""" takes an argument and displays all values states table of hbtn_0e_0_usa """
 
 from sys import argv
 import MySQLdb
