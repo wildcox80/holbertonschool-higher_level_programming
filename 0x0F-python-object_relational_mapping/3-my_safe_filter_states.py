9b88dcb4a33b7b36a7fc4f7fb5ae0daa9d2ed256
Wilder Rincon <1588@holbertonschool.com>
2020-08-08 18:16:20 -0500

    Adding file 3-my_safe_filter_states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" displays all values in the states table of hbtn_0e_0_usa where name """
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    # declaring arguments passed.
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    name = argv[4]
+
+    # Connecting to MySQL database
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+
+    # Making cursor
+    c = db.cursor()
+
+    # Executing query
+    c.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id\
+    ASC".format(argv[4]))
+    query_rows = c.fetchall()
+
+    # Fetching data
+    for row in query_rows:
+        if row[1] == argv[4]:
+            print(row)
+
+    # Close cursor
+    c.close()
+
+    # Close connection database
+    db.close()
