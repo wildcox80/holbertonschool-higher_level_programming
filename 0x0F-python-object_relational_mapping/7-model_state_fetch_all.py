6de5c68d4f3d06e3794de29d397d96fac1f7993d
Wilder Rincon <1588@holbertonschool.com>
2020-08-09 16:16:48 -0500

    Adding file 7-model_state_fetch_all.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" class definition of a State and an instance Base = declarative_base() """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+# Create session mysql sqlalchemy
+Session = sessionmaker(bind=engine)
+session = Session(engine)
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+
+# Close session
+session.close()
