3c13b3ec4c8ce960808455fa40484174a84e4206
Wilder Rincon <1588@holbertonschool.com>
2020-08-09 13:29:31 -0500

    Adding files exercise 6-model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,5 +15,6 @@ Base = declarative_base()
 class State(Base):
     """A class representation of a State."""
     __tablename__ = 'states'
+    
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
