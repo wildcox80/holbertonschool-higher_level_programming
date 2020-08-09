24df2126f74f79ef3396524608954a6f50c48a94
Wilder Rincon <1588@holbertonschool.com>
2020-08-09 16:47:35 -0500

    Adding file 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 Base.metadata.create_all(engine)
 
 # Create session mysql sqlalchemy
-Session = sessionmaker(bind=engine)
+
 session = Session(engine)
 for state in session.query(State).order_by(State.id).all():
     print("{}: {}".format(state.id, state.name))
