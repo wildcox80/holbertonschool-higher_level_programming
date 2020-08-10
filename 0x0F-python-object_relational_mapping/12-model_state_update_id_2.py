ceb4be29a26f0889a6431fc9dd30423eb8f8a312
Wilder Rincon <1588@holbertonschool.com>
2020-08-09 19:31:28 -0500

    Adding file 11-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+A script that adds the State object “Louisiana”
+to the database 'hbtn_0e_6_usa'.
+'''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    # Creating a configured "Session" class.
+    Session = sessionmaker(bind=engine)
+
+    # create a Session.
+    session = Session()
+
+    # selecting state by id.
+    state_select = session.query(State).filter_by(id=2).first()
+    state_select.name = "New Mexico"
+    session.commit()
+
+    session.close()
