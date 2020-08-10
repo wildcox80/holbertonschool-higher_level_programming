3823e3ce94b1c136b226b4741bdb91b11d2a44bc
Wilder Rincon <1588@holbertonschool.com>
2020-08-09 19:40:57 -0500

    Adding file 11-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,11 +13,17 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    Base.metadata.create_all(engine)
+
+    # Creating a configured "Session" class.
+    Session = sessionmaker(bind=engine)
+
+    # create a Session.
     session = Session()
-    state = session.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
+
+    # selecting state by id.
+    state_select = session.query(State).filter_by(id=2).first()
+    state_select.name = "New Mexico"
     session.commit()
 
     session.close()
