6eb6bbfbaaded3d90a47b31ac0f78f9d24c95ebc
Wilder Rincon <1588@holbertonschool.com>
2020-08-18 17:34:15 -0500

    Adding files exercise 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """ Python function to find peak number"""
+
+    lenght = len(list_of_integers)
+    if lenght == 0:
+        return
+    mid = lenght// 2
+    high = list_of_integers[mid]
+    low = list_of_integers[mid - 1]
+
+    if (mid == lenght - 1 or high >= list_of_integers[mid + 1]) and\
+            (mid == 0 or high >= low):
+        return high
+    elif mid != lenght - 1 and list_of_integers[mid + 1] >= high:
+        return (find_peak(list_of_integers[mid:]))
+    return (find_peak(list_of_integers[:mid]))
+
