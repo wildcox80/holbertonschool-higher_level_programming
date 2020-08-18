c447d57ba3d81ba01619df6618d4453a5032050c
Wilder Rincon <1588@holbertonschool.com>
2020-08-18 18:44:07 -0500

    Adding files exercise 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
-    """
-        Finds a peak in a list of unsorted integers
-    """
-    # Edge Cases
+    """ Python function to find peak number"""
 
-    if len(list_of_integers) == 0:
-        return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    if list_of_integers[1] <= list_of_integers[0]:
-        return list_of_integers[0]
-    if list_of_integers[-1] >= list_of_integers[-2]:
-        return list_of_integers[-1]
+    lenght = len(list_of_integers)
+    if lenght == 0:
+        return
+    mid = lenght//2
+    high = list_of_integers[mid]
+    low = list_of_integers[mid - 1]
 
-    # Main recursion
-
-    mid = len(list_of_integers) // 2
-    if list_of_integers[mid] >= list_of_integers[mid - 1] \
-            and list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-
-    if list_of_integers[mid + 1] > list_of_integers[mid]:
-        return(find_peak(list_of_integers[mid + 1:len(list_of_integers)]))
-
-    if list_of_integers[mid - 1] > list_of_integers[mid]:
-        return(find_peak(list_of_integers[0:mid]))
+    if (mid == lenght - 1 or high >= list_of_integers[mid + 1]) and\
+            (mid == 0 or high >= low):
+        return high
+    elif mid != lenght - 1 and list_of_integers[mid + 1] >= high:
+        return (find_peak(list_of_integers[mid:]))
+    return (find_peak(list_of_integers[:mid]))
