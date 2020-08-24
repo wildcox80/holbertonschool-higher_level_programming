c2a7d36b0ef3c7888ba4347f347ec026343dc3c3
Wilder Rincon <1588@holbertonschool.com>
2020-08-24 17:06:18 -0500

    Adding file exercise 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that takes in a URL and an email address, sends a POST request"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
