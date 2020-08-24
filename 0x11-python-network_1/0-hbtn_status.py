1e2d5a2c4b05b140836d37852e2a18769fca2a28
Wilder Rincon <1588@holbertonschool.com>
2020-08-24 16:09:21 -0500

    Adding changes file exercise 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 if __name__ == "__main__":
     resp = "Body response:\n\t- type: {}\n\t-\
-    content: {}\n\t- utf8 content: {}"
+            content: {}\n\t- utf8 content: {}"
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
