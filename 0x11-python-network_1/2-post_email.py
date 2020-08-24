ae033a5f2809602daeb4120b804264f079cf05dd
Wilder Rincon <1588@holbertonschool.com>
2020-08-24 16:46:36 -0500

    Adding file exercise 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Script that takes in a URL and an email"""
 import urllib.request
+import urllib.parse
 import sys
 
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    values = {'email': sys.argv[2]}
+if __name__ == "__main__":
+    my_url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    req = urllib.request.Request(my_url, data)
+    with urllib.request.urlopen(req) as response:
+        message = response.read()
 
-    values = parse.urlencode(values).encode('utf-8')
-    req = request.Request(url, values)
-
-    with request.urlopen(req) as response:
-        the_page = response.read().decode('utf-8')
-        print(the_page)
+        print("{}".format(message.decode('utf-8')))
