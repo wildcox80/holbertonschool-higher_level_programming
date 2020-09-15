854ad38115ac91f39677d4dd02cfa17665e4bc7c
Wilder Rincon <1588@holbertonschool.com>
2020-09-15 16:18:52 -0500

    Adding file exercise 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (isNaN(num) || num < 2) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+console.log(factorial(process.argv[2]));
