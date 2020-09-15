1cf8a9b047b557d3bed2ff8718b71755e458ea52
Wilder Rincon <1588@holbertonschool.com>
2020-09-15 12:03:18 -0500

    Adding file exercise 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/env node
-const c = ' C is fun\n';
-const python = 'Python is cool\n';
-const javascript = 'Javascript is amazing';
-console.log(c, python, javascript);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
