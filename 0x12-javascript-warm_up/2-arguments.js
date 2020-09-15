20039e13a8314e5cbb2bd5f6075911a8bbee7aad
Wilder Rincon <1588@holbertonschool.com>
2020-09-15 11:59:19 -0500

    Adding file exercise 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+let str;
+if (process.argv.length < 3) {
+  str = 'No argument';
+} else if (process.argv.length === 3) {
+  str = 'Argument found';
+} else {
+  str = 'Arguments found';
+}
+console.log(str);
