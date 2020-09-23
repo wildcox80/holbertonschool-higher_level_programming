1f4c6e221013f3dc468221bad343339341dc67bb
Wilder Rincon <1588@holbertonschool.com>
2020-09-22 19:10:05 -0500

    Adding file 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 // retrieves all character names in SW film
 const request = require('request');
-const FILM_URL = `http://swapi.co/api/films/${process.argv[2]}`;
-request(FILM_URL, function (err, res, body) {
+const BASE = 'https://swapi-api.hbtn.io/api/';
+const filmId = 'films/' + process.argv[2];
+request((BASE + filmId, { json: true }, (err, res, body) => {
   if (err) return console.log(err);
 
   body.characters.forEach(e => {
